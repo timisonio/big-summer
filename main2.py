a7e905102564c1f72bc13b4d9317f600e7ba8847
Tim Isonio <timisonio@ucla.edu>
2016-08-16 17:41:18 -0700

    cleaned up

--- main2.py
+++ main2.py
@@ -21,7 +21,8 @@ a = 0.05
 # trait heritability
 h2 = 0.9
 
-m_sample = 1000 # number of people to sample
+# number of people to sample
+m_sample = 1000 
 
 def generate_beta_hats(x, y):
   # x is a 2D numpy array (holding genotypes)
@@ -54,18 +55,8 @@ def generate_phenotypes(betas, genotypes, n, m, h2):
     for ind_index in range(m)
   ]
 
-def generate_yhats(phenotypes, genotypes, n, m, h2):
-  beta_hats = generate_beta_hats(genotypes, phenotypes)
-  return [
-    np.sum(
-      [
-        beta_hats[snp_index]*genotypes[ind_index][snp_index] for snp_index in range(n)
-      ]
-    ) + np.random.normal(0, math.sqrt(1-h2))
-    for ind_index in range(m)
-  ]
-
 def generate_yhats_with_betas(betas, genotypes, n, m, h2):
+  # generate modeled phenotype estimates
     return [
     np.sum(
       [
@@ -75,6 +66,7 @@ def generate_yhats_with_betas(betas, genotypes, n, m, h2):
     for ind_index in range(m)
   ]
 
+# generate genome of SNPs
 mafs = np.random.uniform(a, 1-a, size=n)
 beta_stddev = math.sqrt(h2/n)
 betas = np.random.normal(0, beta_stddev, size=n)
@@ -86,9 +78,9 @@ phenotypes = generate_phenotypes(betas, genotypes, n, m, h2)
 # generate a new pool of individuals not in the larger pool
 new_genotypes = generate_genotypes(mafs, m_sample)
 new_phenotypes = generate_phenotypes(betas, new_genotypes, n, m_sample, h2)
-#new_yhats = generate_yhats(new_phenotypes, new_genotypes, n, m_sample, h2)
 new_yhats = generate_yhats_with_betas(betas, new_genotypes, n, m_sample, h2)
 
+# calculate mean squared and percent error
 new_mse = np.mean([(a - b)**2 for a, b in zip(new_yhats, new_phenotypes)])
 new_mpe = 100 / n * np.sum((a - b)/a for a, b in zip(new_phenotypes, new_yhats))
 
@@ -96,15 +88,12 @@ new_mpe = 100 / n * np.sum((a - b)/a for a, b in zip(new_phenotypes, new_yhats))
 # print("Var[y true]:", np.var(phenotypes))
 # print("New MSE, New MPE: ", new_mse, new_mpe)
 
-
-
 print(new_mse, new_mpe, sep=',', end=',')
 
 
 # pull out some people from the larger pool
 inside_genotypes = genotypes[0:m_sample]
 inside_phenotypes = phenotypes[0:m_sample]
-#inside_yhats = generate_yhats(inside_phenotypes, inside_genotypes, n, m_sample, h2)
 inside_yhats = generate_yhats_with_betas(betas, inside_genotypes, n, m_sample, h2)
 
 inside_mse = np.mean([(a - b)**2 for a, b in zip(inside_yhats, inside_phenotypes)])
@@ -112,15 +101,3 @@ inside_mpe = 100 / n * np.sum((a - b)/a for a, b in zip(inside_phenotypes, insid
 
 # print("Inside MSE, Inside MPE: ", inside_mse, inside_mpe)
 print(inside_mse, inside_mpe, sep=',')
-
-
-
-# beta_hats = generate_beta_hats(genotypes, phenotypes)
-# print(betas[0:5], beta_hats[0:5])
-
-# beta_mse = [(a - b)**2 for a, b in zip(beta_hats, betas)]
-
-# beta_mpe = 100 * np.sum([(a - b)/a for a, b in zip(betas, beta_hats)]) / n
-# print(beta_mpe)
-
-# print(np.mean(beta_mse))
